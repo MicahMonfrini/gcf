42ce2f8f80b2f416f1dbbe4993665795d01ea54c
Micah Monfrini <ffrock905@gmail.com>
2018-09-25 20:47:12 -0500

    comment script.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,11 @@ $(document).ready(function() {
     // Removed .stop method until I know what it does
     // $(this).children(".dropdown-list").stop(true,true).slideToggle("fast")
   });
+  //Hide dropdown if body is clicked
   $("body").click(function(){
     $(".dropdown-list").fadeOut().addClass(".active");
   });
+  //Prevent dropdown click from reaching body
   $(".nav-dropdown").click(function(e){
     e.stopPropagation();
   });

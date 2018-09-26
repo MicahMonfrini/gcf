dad7fa101d29c66bde380411bdbb344ef5391788
Micah Monfrini <ffrock905@gmail.com>
2018-09-25 21:03:17 -0500

    convert nav-dropdown class to two distinct ids

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
 
   // DROPDOWN TOGGLE ONCLICK
-  $(".dropdown-list").hide();
   $(".nav-dropdown").click(function(){
     $(this).children(".dropdown-list").slideToggle("fast")
     // Removed .stop method until I know what it does
     // $(this).children(".dropdown-list").stop(true,true).slideToggle("fast")
   });
   //Hide dropdown if body is clicked
-  $("body").click(function(){
-    $(".dropdown-list").fadeOut().addClass(".active");
-  });
-  //Prevent dropdown click from reaching body
-  $(".nav-dropdown").click(function(e){
-    e.stopPropagation();
+  $(document).on('click', function(event) {
+    if (!$(event.target).closest('.nav-dropdown').length) {
+      $(".dropdown-list").fadeOut();
+    }
   });
 });

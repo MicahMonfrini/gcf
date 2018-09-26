7d11da5816c04f1452adc63482e415cc9757fc06
Micah Monfrini <ffrock905@gmail.com>
2018-09-25 20:45:23 -0500

    hide dropdown on body click

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,14 @@ $(document).ready(function() {
   // DROPDOWN TOGGLE ONCLICK
   $(".dropdown-list").hide();
   $(".nav-dropdown").click(function(){
-      $(this).children(".dropdown-list").stop(true,true).slideToggle("fast")
+    $(this).children(".dropdown-list").slideToggle("fast")
+    // Removed .stop method until I know what it does
+    // $(this).children(".dropdown-list").stop(true,true).slideToggle("fast")
   });
-  // $("body").click(function(){
-  //   $(".nav-dropdown").fadeOut().removeClass(".dropdown-list");
-  // });
+  $("body").click(function(){
+    $(".dropdown-list").fadeOut().addClass(".active");
+  });
+  $(".nav-dropdown").click(function(e){
+    e.stopPropagation();
+  });
 });

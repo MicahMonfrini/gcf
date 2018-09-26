4f430698c3200aecc9be16ba89bfb8efebab7c06
Micah Monfrini <ffrock905@gmail.com>
2018-09-25 22:32:18 -0500

    dropdown toggle functioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
 
   // DROPDOWN TOGGLE ONCLICK
-  $(".nav-dropdown").click(function(){
-    $(this).children(".dropdown-list").slideToggle("fast")
-    // Removed .stop method until I know what it does
-    // $(this).children(".dropdown-list").stop(true,true).slideToggle("fast")
+
+  // .active is set to display: block. Hidden by default
+  $(".active").hide();
+  // Click handler
+  $(".nav-dropdown").click(function() {
+    // Check clicked element for status of .active
+    // If .active is visible on clicked element; fade out
+    if ($(this).find(".active").is(":visible")) {
+      $(".active").fadeOut()
+    // If .active is not visible on clicked element...
+    } else {
+      // all visible .active classes fade out
+      $(".active").fadeOut();
+      // fade in .active on clicked element
+      $(this).find(".active").fadeIn();
+    }
   });
+
   //Hide dropdown if body is clicked
   $(document).on('click', function(event) {
     if (!$(event.target).closest('.nav-dropdown').length) {

a4648090f8f93c0c39958c03cdf636667c3ada07
Micah Monfrini <ffrock905@gmail.com>
2018-09-27 23:11:48 -0500

    dropdown list - speed up fadein/out effect

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,20 @@ $(document).ready(function() {
     // Check clicked element for status of .active
     // If .active is visible on clicked element; fade out
     if ($(this).find(".active").is(":visible")) {
-      $(".active").fadeOut()
+      $(".active").fadeOut(250)
     // If .active is not visible on clicked element...
     } else {
       // all visible .active classes fade out
-      $(".active").fadeOut();
+      $(".active").fadeOut(250);
       // fade in .active on clicked element
-      $(this).find(".active").fadeIn();
+      $(this).find(".active").fadeIn(250);
     }
   });
 
   //Hide dropdown if body is clicked
   $(document).on('click', function(event) {
     if (!$(event.target).closest('.nav-dropdown').length) {
-      $(".dropdown-list").fadeOut()
+      $(".dropdown-list").fadeOut(250)
     }
   });
 });
